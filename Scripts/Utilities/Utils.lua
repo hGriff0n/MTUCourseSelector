546dac313a712bbcf2b6791582eb8b2c1540d9ae
hGriff0n <ghooper96@gmail.com>
2014-12-08 14:53:36 -0500

    Updated Range Iterator
    Changed Range Iterator to stop on passing a value
    Previous behavior was to stop on hitting the value
    Also added a couple includes for future files

--- Scripts/Utilities/Utils.lua
+++ Scripts/Utilities/Utils.lua
@@ -1,7 +1,9 @@
 require "Scripts/Utilities/File"
 require "Scripts/Utilities/Output"
+require "Scripts/Utilities/Overloads"
 require "Scripts/Utilities/String"
 require "Scripts/Utilities/Table"
+require "Scripts/Utilities/Various"
 
 range = function(i, to, inc)
 	if not i then return end
@@ -11,13 +13,19 @@ range = function(i, to, inc)
 		i = to == 0 and 0 or (to > 0 and 1 or -1)
 	end
 
+	local abs = function(x) return x < 0 and -x or x end		-- for some reason math.abs is sometimes nil ???
+
 	inc = inc or (i < to and 1 or -1)
 
 	i = i - inc
 
+	local s = (to - i) / abs(to - i)
+
 	return function()
-				if i == to then return nil end
 				i = i + inc
+
+				if (s * (to - i)) ~= abs(to - i) then return nil end
+
 				return i, i --return i, inc
 		   end
 end
