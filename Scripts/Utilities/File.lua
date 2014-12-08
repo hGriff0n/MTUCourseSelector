1074d314b62268eea7ad64dbce8dd1a77b758d7b
hGriff0n <ghooper96@gmail.com>
2014-12-08 15:50:47 -0500

    File Iterators
    Added iterator to loop over all files in a directory
    Added sifter to restrict the files to a specific extension
    Added table utility to write to a file
    Added function to read all file data in a directory

--- Scripts/Utilities/File.lua
+++ Scripts/Utilities/File.lua
@@ -1,18 +1,43 @@
 
 filepath = "C:/Users/Grayson/Documents/Polycode/CourseSelector/"
 
-function open(filename)
-	return io.open(filename, "r")
+function readAll(loc, sifter)
+	local sift, t = fileExtension(sifter or ""), {}
+
+	for file in directory(loc, sift) do
+		local input = io.open(loc .. "/" .. file, "r")
+		t[#t + 1] = input:read("*a")
+		input:close()
+	end
+
+	return t
 end
 
---fileExists
+function allFiles(directory)
+	local t = {}
 
--- rename ???
-function wipe(filename)
-	io.open(filename, "w"):close()
+	for file in io.popen([[dir "]] .. directory .. [[" /b]]):lines() do
+		t[file] = true
+	end
+
+	return t
 end
 
--- lua-users.org/wiki/IoLibraryTutorial
-function read(file)
-	return file:read()
+-- directory iterator
+function directory(loc, sifter)
+	local sift = sifter and function(v, i) return not sifter(i) end or function() end
+	return pairs(table.removeIf(allFiles(loc), sift))
+end
+
+-- directory sifters
+function fileExtension(ext)
+	return function(file) return file:find(ext) end
+end
+
+table.toFile = function(t, file, mode)
+	local f = io.open(file, mode or "w")
+
+	f:write(table.tostring(t):gsub("[\n]+", "\n"))
+
+	f:close()
 end
