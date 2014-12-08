fa2cb1c4d0d3d89a741d3712553309a1f85ddfd3
hGriff0n <ghooper96@gmail.com>
2014-12-08 15:54:04 -0500

    Course
    Filled out the course class with data and functions
    Methods: dependsOn, studentMeetsRequirements, glean, getSplit,
    creditHours
    Additional data can be accessed directly with knowledge of the course
    data names, may add accessors later on
    Function: makeCourse to make a course from a HTMLDoc

--- Scripts/Structures/Course.lua
+++ Scripts/Structures/Course.lua
@@ -1,24 +1,68 @@
 require "Scripts/Structures/Requirements"
-require "Scripts/Utilities/String"
 
 class "Course"
 
-function Course:Course(course, credits, fee, dates, reqs)
-	self.department, self.number = table.unpack(course[1]:split())
+function Course:Course(title, data)
+	self.title = title
 
-	self.title = course[2]
-	self.description = course[3]
+	if data then
+		self.desc = data[1][1]
 
-	self.credit_split = credits
-	self.linked_lab = nil
-	self.fee = fee
+		self.credits = table.generate(table.removeIf(data, siftCredits), function(v) return v[2]:substr(2) end, function(i, v) return v[1] end)
 
-	self.date = dates
-
-	self.requirements = getRequirements(course, reqs)
-	self.other_data = {}
+		self.requirements = Requirement(table.removeIf(data, siftReqs))
+	end
 end
 
-function Course:name()
-	return self.department:upper() .. self.number
+function Course:__eq(c2)
+	if type(c2) == type(self) then		-- might want to change to allow comparisons with Section
+		return self.title == c2.title or self.course == c2.title or self.course == c2.course
+	end
+end
+
+function Course:creditHours()
+	if type(self.credits.Credits) == "string" then
+		self.credits.Credits = tonumber(self.credits.Credits)
+	end
+
+	return self.credits.Credits
+end
+
+function Course:getSplit()
+	if not self.split then
+		local s = { "Lec", "Rec", "Lab" }
+		self.split = table.generate(self.credits["Lec-Rec-Lab"]:substr(2, -2):sep("-", true), function(v) return tonumber(v) end, function(i) return s[i] end)
+	end
+
+	return self.split
+end
+
+function Course:glean(classes)
+	self.course = classes[1]:course()
+end
+
+function Course:studentMeetsRequirements(student)
+	return student:passes(self.requirements)
+end
+
+function Course:dependsOn(course)
+	if type(course) == "Course" or type(course) == "Section" then
+		course = course:course()
+	end
+
+	return table.findIn(self.requirements:prereqs(), course) or table.findIn(self.requirements:coreqs(), course)
+end
+
+
+
+function makeCourse(course)
+	local title = course:getContent()
+	local t = table.map(table.map(course:subNodes(), getContent), function(v) return v:sep("[:;]", true) end)
+
+	return Course(course:getContent(), table.removeIf(t, function(v) return v[1] == "" end))
+end
+
+
+function siftCredits(v)
+	return not (v[1] == "Credits" or v[1] == "Lec-Rec-Lab")
 end
